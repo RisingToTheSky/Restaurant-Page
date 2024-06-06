1ee1ea9aad5f87c4eba246f777218ed3986226b5
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-06-06 16:18:02 +0200

    Add source map to configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   watch: true,
   output: {
     filename: 'main.js',

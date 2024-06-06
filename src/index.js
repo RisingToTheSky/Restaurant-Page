c604a1520dc0fc4cbf77a6b794fbf52ab2248c04
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-06-06 21:17:59 +0200

    Add an eventlistener to navbar buttons
    It switches between the 3 button options

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import loadHome from './home.js';
+import loadMenu from './menu.js';
+import loadContact from './contact.js';
+import './style.css';
 
 function initialPageLoad() {
     loadHome;
 }
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        content.innerHtml = "";
+        if (button.textContent === "Home") {
+            loadHome;
+        } else if (button.textContent === "Menu") {
+            loadMenu;
+        } else if (button.textContent === "Contact") {
+            loadContact;
+        }
+    })
+})
 
 initialPageLoad();

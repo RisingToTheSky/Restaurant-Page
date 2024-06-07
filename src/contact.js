fb2080464b2d6b7c7b03038ec6f66db12a4a5e8e
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-06-07 13:34:43 +0200

    Add contact content

---
+++ src/contact.js
@@ -0,0 +1,36 @@
+import location from './images/location.png';
+
+function createContact() {
+    const contact = document.createElement("div");
+    contact.classList.add("contact");
+
+    const contactInfo = document.createElement("div");
+    contactInfo.classList.add("contactList");
+    contact.appendChild(contactInfo);
+
+    const locationImage = new Image();
+    locationImage.src = location;
+    locationImage.classList.add("location");
+
+    contactInfo.appendChild(locationImage);
+    contactInfo.appendChild(createParagraph("Address: Via Cipro, 21"));
+    contactInfo.appendChild(createParagraph("City: Rome"));
+    contactInfo.appendChild(createParagraph("State: Lazio"));
+    contactInfo.appendChild(createParagraph("Phone number: +39 6456463202"));
+
+    return contact;
+}
+
+function createParagraph(text) {
+    const p = document.createElement("p");
+    p.textContent = text;
+    return p;
+}
+
+function loadContact() {
+    const content = document.getElementById("content");
+    content.appendChild(createContact());
+    return content;
+}
+
+export default loadContact();
